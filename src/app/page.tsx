5542a186ac0e0f2589910dfeb740c6c8c4d9f331
Daniel Broe <BroeDaniel@gmail.com>
2023-07-27 15:47:41 +0200

    adding hero section to / route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,10 @@
-import Image from 'next/image';
 import styles from './page.module.css';
+import HeroSection from '@/components/hero';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'>
-            By{' '}
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <h1>TenderTask</h1>
-      </div>
-
-      <div className={styles.grid}>
-        <div>Footer 1</div>
-        <div>Footer 2</div>
-        <div>Footer 3</div>
-        <div>Footer 4</div>
-      </div>
+      <HeroSection />
     </main>
   );
 }
