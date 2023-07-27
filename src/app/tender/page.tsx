f76ba00d3739134e00059934cdce787cc53ef3f0
Daniel Broe <BroeDaniel@gmail.com>
2023-07-27 15:48:14 +0200

    adding tender page

---
+++ src/app/tender/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function page() {
+  return (
+    <div>
+      This is the tender <Link href='/'>Home</Link>
+    </div>
+  );
+}
